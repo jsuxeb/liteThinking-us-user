f2fe8fe026e8b9082f54037e4e9e50310ecb98f1
jeyser <jeyser.suxe@gmail.com>
2024-01-06 20:03:24 -0500

    se agrega codigo de errores 400,500,200,404

--- src/main/java/com/litethinking/msuser/controller/GlobalExceptionHandler.java
+++ src/main/java/com/litethinking/msuser/controller/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.litethinking.msuser.controller;
 
 import com.litethinking.msuser.exceptionhandler.ResourceNotFoundException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,9 +13,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Slf4j
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseBody
+    public ResponseEntity<String> sqlException(DataIntegrityViolationException ex) {
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("Error en el REQUEST: " + ex.getMessage());
+    }
+
     @ExceptionHandler(Exception.class)
     @ResponseBody
-    public ResponseEntity<String> handlerExcetion(Exception ex) {
+    public ResponseEntity<String> handlerException(Exception ex) {
         log.error("Se produjo un error no controlado a nivel del servidor", ex);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error interno de servidor");
     }
